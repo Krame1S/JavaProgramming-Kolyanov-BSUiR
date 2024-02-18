34cc21fc74e3902e79e43fa55da7c4608396c37a
Ilya <ilya@lenovolm>
2024-02-18 20:29:39 +0300

    Some encapsulation logic added New class UrlChecker UrlCheckService class now uses an instance of UrlChecker class

--- src/main/java/com/lab1/isthesiteup/controllers/UrlCheckController.java
+++ src/main/java/com/lab1/isthesiteup/controllers/UrlCheckController.java
@@ -1,18 +1,16 @@
 package com.lab1.isthesiteup.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lab1.isthesiteup.services.UrlCheckService;
 
-@RestController
+@RestController                                                     //This annotation is used to mark a class as a request handling component 
 public class UrlCheckController {
 
     private final UrlCheckService urlCheckService;
 
-    @Autowired
     public UrlCheckController(UrlCheckService urlCheckService) {
         this.urlCheckService = urlCheckService;
     }
--- src/main/java/com/lab1/isthesiteup/services/UrlCheckService.java
+++ src/main/java/com/lab1/isthesiteup/services/UrlCheckService.java
@@ -1,36 +1,22 @@
 package com.lab1.isthesiteup.services;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlCheckService {
 
-    private static final String SITE_IS_UP = "Site is up!";
-    private static final String SITE_IS_DOWN = "Site is down!";
-    private static final String INCORRECT_URL = "URL is incorrect!";
+    private final UrlChecker urlChecker;
+
+    public UrlCheckService(UrlChecker urlChecker) {
+        this.urlChecker = urlChecker;
+    }
 
     public String checkUrl(String url) {
-        String returnMessage = "";
         try {
-            URL urlObj = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-            int responseCode = conn.getResponseCode();
-            if (responseCode ==  200) {
-                returnMessage = SITE_IS_UP;
-            } else {
-                returnMessage = SITE_IS_DOWN;
-            }
-        } catch (MalformedURLException e) {
-            returnMessage = INCORRECT_URL;
+            return urlChecker.check(url);
         } catch (IOException e) {
-            returnMessage = SITE_IS_DOWN;
+            return "Site is down!";
         }
-        return returnMessage;
     }
 }
