d805c359a7feba9f44685500cdaa1dd48e8d217b
Ilya <ilya@lenovolm>
2024-02-20 15:46:53 +0300

    Incapsulation logic removed

--- src/main/java/com/lab1/isthesiteup/controllers/UrlCheckController.java
+++ src/main/java/com/lab1/isthesiteup/controllers/UrlCheckController.java
@@ -1,6 +1,5 @@
 package com.lab1.isthesiteup.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ public class UrlCheckController {
 
     private final UrlCheckService urlCheckService;
 
-    @Autowired
     public UrlCheckController(UrlCheckService urlCheckService) {
         this.urlCheckService = urlCheckService;
     }
