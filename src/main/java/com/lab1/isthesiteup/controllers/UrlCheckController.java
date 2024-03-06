7b5779a7fce7110153600d59fda2afac2ef646f3
Ilya <ilya@lenovolm>
2024-03-07 00:29:14 +0300

    Some CRUD operations added⚒️

--- src/main/java/com/lab1/isthesiteup/controllers/UrlCheckController.java
+++ src/main/java/com/lab1/isthesiteup/controllers/UrlCheckController.java
@@ -1,10 +1,15 @@
 package com.lab1.isthesiteup.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lab1.isthesiteup.entities.UrlCheckEntity;
 import com.lab1.isthesiteup.services.UrlCheckService;
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +36,19 @@ public class UrlCheckController {
         return "check";
     }
 
-        
+    @GetMapping("/all-checks")
+    public String showAllUrlChecks(Model model) {
+        List<UrlCheckEntity> allUrlChecks = urlCheckService.getAllUrlChecks();
+        model.addAttribute("allUrlChecks", allUrlChecks);
+        return "all-checks";
+    }
+
+    @DeleteMapping("/all-checks/{id}")
+    public String deleteUrlCheck(@PathVariable Long id) {
+        urlCheckService.deleteUrlCheck(id);
+        return "redirect:/all-checks";
+    }
+
+
 
 }
