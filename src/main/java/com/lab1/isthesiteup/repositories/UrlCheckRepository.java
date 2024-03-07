341e9d88ca2afd5a8ae8242a3e7706eac38c4ac1
Ilya <ilya@lenovolm>
2024-03-07 14:52:33 +0300

    New method added🆕​

--- src/main/java/com/lab1/isthesiteup/repositories/UrlCheckRepository.java
+++ src/main/java/com/lab1/isthesiteup/repositories/UrlCheckRepository.java
@@ -1,8 +1,11 @@
 package com.lab1.isthesiteup.repositories;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.lab1.isthesiteup.entities.UrlCheckEntity;
 
 public interface UrlCheckRepository extends JpaRepository<UrlCheckEntity, Long> {
-    
+    List<UrlCheckEntity> findByTimeBetween(LocalDateTime start, LocalDateTime end);
 }
