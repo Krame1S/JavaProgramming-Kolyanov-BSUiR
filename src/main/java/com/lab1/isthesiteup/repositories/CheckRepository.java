40fb20a23767239e60906cdf1707c580071d17ea
iliaha2005@gmail.com <iliaha2005@gmail.com>
2024-03-20 19:28:35 +0300

    Second lab finished🏁

--- src/main/java/com/lab1/isthesiteup/repositories/CheckRepository.java
+++ src/main/java/com/lab1/isthesiteup/repositories/CheckRepository.java
@@ -1,9 +1,12 @@
 package com.lab1.isthesiteup.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.lab1.isthesiteup.entities.CheckEntity;
 
 @Repository
 public interface CheckRepository extends JpaRepository<CheckEntity, Long> {
+    List<CheckEntity> findByServerEntityUrl(String url);
 }
