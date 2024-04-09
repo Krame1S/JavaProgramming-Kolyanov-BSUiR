28d1089395482fd73f3ce875f4a25f415b783ad1
iliaha2005@gmail.com <iliaha2005@gmail.com>
2024-04-09 13:13:41 +0300

    typo

--- src/test/java/com/lab1/isthesiteup/IsTheSiteUpApplicationTests.java
+++ src/test/java/com/lab1/isthesiteup/IsTheSiteUpApplicationTests.java
@@ -1,13 +1,7 @@
 package com.lab1.isthesiteup;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IsTheSiteUpApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
